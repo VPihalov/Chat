9f8fd1fe8f6637b631d17df8f81fadafe0713306
Valerii Pihalov <valerii.pihalov@globallogic.com>
2021-07-27 21:42:19 +0300

    add api swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
+    @ApiProperty({
+        description: `This is user login`,
+        required: true
+    })
     @IsNotEmpty()
     @MinLength(4)
     @MaxLength(10)
     username: string;
      
+    @ApiProperty()
     @IsNotEmpty()
     @MinLength(4)
     @MaxLength(10)
