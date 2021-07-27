530e1975fbbf233e679bea0475ea16e2f7c0a3b2
Valerii Pihalov <valerii.pihalov@globallogic.com>
2021-07-27 13:44:06 +0300

    Validation for params added

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, MaxLength, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsNotEmpty()
+    @MinLength(4)
+    @MaxLength(10)
     username: string;
+     
+    @IsNotEmpty()
+    @MinLength(4)
+    @MaxLength(10)
     password: string;
 }
