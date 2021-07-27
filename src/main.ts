3cb53be2ecc5795a7b3838103bfdedc749477111
Valerii Pihalov <valerii.pihalov@globallogic.com>
2021-07-27 20:05:40 +0300

    feat env config create

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
